86ca73f74e2af58d64b253a160460bbf219ad17e
Dobuss <dobromira.ivanova911@gmail.com>
2023-11-02 16:21:06 +0200

    first use of refs

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,20 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const input = useRef();
+
+  const [playerName, setPlayerName] = useState("");
+
+  function handleClick() {
+    setPlayerName(input.current.value);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {playerName ?? "unknown entity"}</h2> {/*input first value if true, else second*/ }
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={input} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
