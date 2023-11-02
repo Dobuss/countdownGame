ba8e677bd194608911139343caf72f5b71720c26
Dobuss <dobromira.ivanova911@gmail.com>
2023-11-02 17:34:17 +0200

    timer implementation

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,14 +1,38 @@
-export default function TimerChallenge({title, targetTime}){
-    return <section className="challenge">
-        <h2>{title}</h2>
-        <p className="challenge-time">
-        {targetTime} second{targetTime > 1 ? 's' : ''}
-        </p>
-        <p>
-            <button>Start Challenge</button>
-        </p>
-        <p className="">
-            Time is running / Time innactive
-        </p>
+import { useRef, useState } from "react";
+
+export default function TimerChallenge({ title, targetTime }) {
+  const [timerExpired, setTimerExpired] = useState(false);
+  const [timerStarted, setTimerStarted] = useState(false);
+
+  const timer = useRef();
+
+  function handleStart() {
+    setTimerStarted(true);
+    timer.current = setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+  }
+
+  function handleStop() {
+    clearTimeout(timer.current);
+    setTimerStarted(false);
+  }
+
+  return (
+    <section className="challenge">
+      <h2>{title}</h2>
+      {timerExpired && <p>You lost!</p>}
+      <p className="challenge-time">
+        {targetTime} second{targetTime > 1 ? "s" : ""}
+      </p>
+      <p>
+        <button onClick={timerStarted ? handleStop : handleStart}>
+          {timerStarted ? "Stop" : "Start"} Challenge
+        </button>
+      </p>
+      <p className={timerStarted ? "active" : undefined}>
+        {timerStarted ? "Time is running..." : "Time innactive"}
+      </p>
     </section>
+  );
 }
