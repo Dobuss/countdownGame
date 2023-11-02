a393cc78c919377e10ef8e3ce92796ccd877757a
Dobuss <dobromira.ivanova911@gmail.com>
2023-11-02 18:16:02 +0200

    impl useImperativeHandle hook

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -12,7 +12,7 @@ export default function TimerChallenge({ title, targetTime }) {
     setTimerStarted(true);
     timer.current = setTimeout(() => {
       setTimerExpired(true);
-      dialog.current.showModal();
+      dialog.current.open();
     }, targetTime * 1000);
   }
 
